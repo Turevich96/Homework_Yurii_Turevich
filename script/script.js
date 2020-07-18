9180bebc5d1473bc0cab28a48c28cc7ac817a3a4
Yura Turevich <oleo.ukraine@gmail.com>
2020-07-19 00:11:53 +0300

    add section

---
+++ script/script.js
@@ -0,0 +1,23 @@
+'use strict';
+// slider_1
+$('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+
+    autoplaySpeed: 5000,
+    infinite: true,
+    // autoplay: true,
+    fade: true,
+    cssEase: 'linear'
+
+});
+
+// slider_2
+$('.latest-news-content__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});

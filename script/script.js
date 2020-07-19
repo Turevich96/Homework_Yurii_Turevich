9886d39fa77eded37f268f3cdc25fecb153030a7
Yura Turevich <oleo.ukraine@gmail.com>
2020-07-19 20:28:22 +0300

    add footer

--- script/script.js
+++ script/script.js
@@ -1,17 +1,19 @@
 'use strict';
 // slider_1
 $('.slick-slider').slick({
+    autoplay: true,
     dots: true,
     infinite: true,
     speed: 400,
     slidesToShow: 1,
     adaptiveHeight: true,
 
-    autoplaySpeed: 5000,
+    autoplaySpeed: 4000,
     infinite: true,
-    // autoplay: true,
+
     fade: true,
     cssEase: 'linear'
+
 
 });
 
@@ -20,4 +22,201 @@ $('.latest-news-content__slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1
+});
+
+// google map 
+// Initialize and add the map
+function initMap() {
+    // Styles a map in night mode.
+
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.674, lng: -73.945 },
+        zoom: 12,
+        disableDefaultUI: true,
+        styles: [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#f5f5f5"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#bdbdbd"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.attraction",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ffffff"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#757575"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dadada"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#616161"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e5e5e5"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#eeeeee"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#c9c9c9"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9e9e9e"
+                    }
+                ]
+            }
+        ]
+    });
+
+}
+
+// Scroll transition 
+jQuery(function ($) {
+    $('a[href*="#"]').on('click.smoothscroll', function (e) {
+        var hash = this.hash, _hash = hash.replace(/#/, ''), theHref = $(this).attr('href').replace(/#.*/, '');
+        if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+        var $target = _hash === '' ? $('body') : $(hash + ', a[name="' + _hash + '"]').first();
+        if (!$target.length) return;
+        e.preventDefault();
+        $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 400, 'swing', function () {
+            window.location.hash = hash;
+        });
+    });
 });

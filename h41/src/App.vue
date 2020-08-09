3c7025fe347a32b179436b412b5460fd136bc26a
Yura Turevich <oleo.ukraine@gmail.com>
2020-08-09 16:22:10 +0300

    finish dz

---
+++ h41/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <InputText />
+    <Part2Dz />
+  </div>
+</template>
+
+<script>
+import InputText from "./components/InputText.vue";
+import Part2Dz from "./components/Part2Dz.vue";
+
+export default {
+  name: "App",
+  components: {
+    InputText,
+    Part2Dz,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ h41/src/components/InputText.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent="getText" class="text">
+    <input v-model="printText" type="text" name id class="text__input" size="100" />
+    <button type="submit" class="text__button">OK</button>
+    <p class="text__text-block" v-bind:class="{ bgRed:isRed, bgYellow: isYellow}">{{print}}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      printText: "",
+      print: "",
+      isRed: false,
+      isYellow: false,
+      isBlu: false,
+    };
+  },
+
+  methods: {
+    getText() {
+      // print text in document and reset text in input
+      this.print = this.printText;
+      this.printText = "";
+
+      //   set bg color for text-box
+      if (this.print.length > 0) {
+        this.isRed = true;
+        this.isYellow = false;
+      }
+      if (this.print.length > 5) {
+        this.isYellow = true;
+        this.isRed = false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.text {
+  &__input {
+    widows: 400px;
+    height: 40px;
+    font-size: 24px;
+  }
+  &__button {
+    margin: 30px;
+    height: 40px;
+    padding: 10px;
+  }
+  &__text-block {
+    font-size: 30px;
+  }
+}
+.bgRed {
+  background-color: red;
+}
+.bgYellow {
+  background-color: yellow;
+}
+</style>

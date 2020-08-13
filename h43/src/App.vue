a7e202a42422fe160d9959822949f72cc1372786
Yura Turevich <oleo.ukraine@gmail.com>
2020-08-13 17:17:24 +0300

    finish

---
+++ h43/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Numb />
+  </div>
+</template>
+
+<script>
+import Numb from "./components/Numb.vue";
+
+export default {
+  name: "App",
+  components: {
+    Numb,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
